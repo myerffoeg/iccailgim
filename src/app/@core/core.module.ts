536086d5b43a3f041971f16b1b18d1ad34e54727
Geoffrey Migliacci <geoffrey.migliacci@ellyos.fr>
2020-10-07 13:18:53 +0200

    feat(analytics): adding google analytics

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,11 +1,17 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 import 'firebase/firestore';
 import { environment } from 'src/environments/environment';
 
 @NgModule({
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule
+  ],
+  providers: [
+    ScreenTrackingService,
+    UserTrackingService
   ]
 })
 export class CoreModule {
