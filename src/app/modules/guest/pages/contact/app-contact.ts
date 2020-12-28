d9860ba0eada1381f78481c121ff2683e5c8342a
Geoffrey <geoffrey.migliacci@ellyos.fr>
2020-12-28 16:13:14 +0100

    feat(contact): tooltips on contacts

--- src/app/modules/guest/pages/contact/app-contact.ts
+++ src/app/modules/guest/pages/contact/app-contact.ts
@@ -2,22 +2,27 @@ export interface AppContact {
     type: string;
     icon: string;
     link: string;
+    tooltip: string;
 }
 
 export const CONTACT_ITEMS: AppContact[] = [
     {
         type: 'fas',
         icon: 'paper-plane',
-        link: 'mailto:geoffrey.migliacci@gmail.com'
+        link: 'mailto:geoffrey.migliacci@gmail.com',
+        tooltip: 'E-mail'
     },
     {
         type: 'fas',
         icon: 'phone-alt',
-        link: 'tel:+33618514026'
+        link: 'tel:+33618514026',
+        tooltip: 'Téléphone'
+
     },
     {
         type: 'fab',
         icon: 'linkedin-in',
-        link: 'https://www.linkedin.com/in/geoffrey-migliacci/'
+        link: 'https://www.linkedin.com/in/geoffrey-migliacci/',
+        tooltip: 'LinkedIn'
     }
 ];
