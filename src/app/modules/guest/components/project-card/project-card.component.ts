3d8515d31dc082f6db0b2489cc2c18fe5b899006
Geoffrey <geoffrey.migliacci@ellyos.fr>
2021-01-06 19:52:28 +0100

    feat(storage): only admin

--- src/app/modules/guest/components/project-card/project-card.component.ts
+++ src/app/modules/guest/components/project-card/project-card.component.ts
@@ -4,8 +4,7 @@ import { Project } from 'src/app/stores/project';
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./project-card.component.scss']
 })
 export class ProjectCardComponent implements OnInit {
 
